db7aafec96ec0d378557e64c7740d0cd40789b4d
Dora Sun <lobster1216@gmail.com>
2017-12-08 22:33:48 -0500

    Making the game horizontal instead

--- src/sketch.js
+++ src/sketch.js
@@ -19,6 +19,7 @@ var x1 = 150;
 var y1 = 250;
 var x2 = 350;
 var y2 = 250;
+var lineHeight = 525;
 
 var squares = [];
 var circles = [];
@@ -33,7 +34,7 @@ function preload(){
 }
 
 function setup() {
-  canvas = createCanvas(1024, 500);
+  canvas = createCanvas(1024, 600);
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   canvas.position(x, y);
@@ -55,7 +56,6 @@ function setup() {
 
 function draw() {
   background(0);
-
 
   if(gameMode === 0){
     startScreen();
@@ -91,10 +91,10 @@ function game(){
     if(i/16 < 1){
       fill(241,145,129, 80);
       
-       if(counterSquare < countdown && pushSquare){
+      if(counterSquare < countdown && pushSquare){
         if(amp === 225){
           console.log("Push");
-          squares.push(new Square());
+          squares.push(new Square(0));
           pushSquare = false;
         }
       }
@@ -109,10 +109,29 @@ function game(){
     }
     else if (i/16 < 2){
       fill(243,245,196, 80);
-     
+
+      if(counterSquare < countdown && pushSquare){
+        if(amp === 225){
+          console.log("Push");
+          squares.push(new Square(1));
+          pushSquare = false;
+        }
+      }
+
+      if(counterSquare >= countdown){
+        counterSquare = 0;
+        pushSquare = true;
+        // if(amp < 30){
+        //   pushSquare = true;
+        // }
+      }
+    }
+    else if (i/16 < 3){
+      fill(147,237,212, 80);
+
       if(counterCircle < countdown && pushCircle){
         if(amp === 225){
-          circles.push(new Circle());
+          circles.push(new Circle(0));
           pushCircle = false;
         }
       }
@@ -124,27 +143,39 @@ function game(){
         // }
       }
     }
-    else if (i/16 < 3){
-      fill(147,237,212, 80);
-    }
     else{
       fill(60,186,200, 80);
-    }
 
+      if(counterCircle < countdown && pushCircle){
+        if(amp === 225){
+          circles.push(new Circle(1));
+          pushCircle = false;
+        }
+      }
+      if(counterCircle >= countdown){
+        counterCircle = 0;
+        pushCircle = true;
+        // if(amp < 30){
+        //   pushSquare = true;
+        // }
+      }
+    }
+    rectMode(CORNER);
     rect(i * w, y, w-2, height - y);
   }
 
   fill(0);
   stroke(255);
   strokeWeight(5);
-  line(100, 0, 100, height);
+  line(0, lineHeight, width, lineHeight);
 
+  rectMode(CENTER);
   strokeWeight(1);
   fill(255,0,0);
-  ellipse(100, y1, 25, 25);
+  rect(x1, lineHeight, 25, 25);
 
   fill(0,255,0);
-  ellipse(100, y2, 25, 25);
+  ellipse(x2, lineHeight, 25, 25);
 
   for(var i =0;i<squares.length;i++){
     squares[i].display();
@@ -244,20 +275,31 @@ function playSound(){
   }
 }
 
-function Square(){
-  this.positionX = width;
-  this.positionY = 50;
-  this.speedX = 10;
+function Square(state){
+  if(state == 0){
+    this.positionX = 128;
+  }
+  else{
+    this.positionX = 384;
+  }
+  this.positionY = 0;
+  this.speedY = 7;
 
   this.move = function(){
-    this.positionX -= this.speedX;
-    if(this.positionX <= 0){
+    this.positionY += this.speedY;
+    if(this.positionY >= height){
       squares.splice(0, 1);
     }
   }
 
   this.display = function(){
-    fill(241,145,129);
+    rectMode(CENTER);
+    if(state == 0){
+      fill(241,145,129);
+    }
+    else{
+      fill(243,245,196);
+    }
     rect(this.positionX, this.positionY, 50, 50);
   }
 
@@ -272,38 +314,30 @@ function Square(){
   }
 }
 
-// function Triangle(){
-//   this.positionX = width;
-//   this.positionY = 50;
-//   this.speedX = 10;
-
-//   this.move = function(){
-//     this.positionX -= this.speedX;
-//     if(this.positionX <= 0){
-//       squares.splice(0, 1);
-//     }
-//   }
-
-//   this.display = function(){
-//     fill(255, 0, 0);
-//     rect(this.positionX, this.positionY, 50, 50);
-//   }
-// }
-
-function Circle(){
-  this.positionX = width;
-  this.positionY = 200;
-  this.speedX = 10;
+function Circle(state){
+  if(state == 0){
+    this.positionX = 640;
+  }
+  else{
+    this.positionX = 896;
+  }
+  this.positionY = 0;
+  this.speedY = 7;
 
   this.move = function(){
-    this.positionX -= this.speedX;
-    if(this.positionX <= 0){
+    this.positionY += this.speedY;
+    if(this.positionY >= height){
       circles.splice(0, 1);
     }
   }
 
   this.display = function(){
-    fill(243,245,196);
+    if(state == 0){
+      fill(147,237,212);
+    }
+    else{
+      fill(60,186,200);
+    }
     ellipse(this.positionX, this.positionY, 50, 50);
   }
 
